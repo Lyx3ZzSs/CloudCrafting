6174d048fea6f18fbb8262446b64044220950234
Lyx3ZzSs <lyx971228@163.com>
2025-01-15 15:27:04 +0800

    fix：隔离-FixedThreadPoolBulkhead（固定线程池舱壁）

--- cloud-consumer-feign-order80/src/main/java/com/sprixin/cloud/controller/OrderCircuitController.java
+++ cloud-consumer-feign-order80/src/main/java/com/sprixin/cloud/controller/OrderCircuitController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class OrderCircuitController {
     @Resource
@@ -25,12 +28,18 @@ public class OrderCircuitController {
     }
 
     @GetMapping("/feign/pay/bulkhead/{id}")
-    @Bulkhead(name = "cloud-payment-service",fallbackMethod = "myBulkheadFallback",type = Bulkhead.Type.THREADPOOL)
-    public String myBulkhead(@PathVariable("id")Integer id) {
-        return payFeignApi.myBulkhead(id);
+    @Bulkhead(name = "cloud-payment-service",fallbackMethod = "myBulkheadPoolFallback",type = Bulkhead.Type.THREADPOOL)
+    public CompletableFuture<String> myBulkheadTHREADPOOL(@PathVariable("id") Integer id)
+    {
+        System.out.println(Thread.currentThread().getName()+"\t"+"enter the method!!!");
+        try { TimeUnit.SECONDS.sleep(3); } catch (InterruptedException e) { e.printStackTrace(); }
+        System.out.println(Thread.currentThread().getName()+"\t"+"exist the method!!!");
+
+        return CompletableFuture.supplyAsync(() -> payFeignApi.myBulkhead(id) + "\t" + " Bulkhead.Type.THREADPOOL");
     }
 
-    public String myBulkheadFallback(Throwable t) {
-        return "myBulkheadFallback,隔板超出最大数量限制，系统繁忙，请稍后再试(*^▽^*)";
+    public CompletableFuture<String> myBulkheadPoolFallback(Integer id, Throwable t)
+    {
+        return CompletableFuture.supplyAsync(() -> "Bulkhead.Type.THREADPOOL，系统繁忙，请稍后再试-----/(ㄒoㄒ)/~~");
     }
 }
